2eb3bc8564968c4c9285585b76f603b428090c22
Olivier Duhart <olivier.duhart@sage.com>
2021-10-18 12:27:34 +0200

    cleaning

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
 
 import {calculateDays} from './time.js'
 
-	export let name;
-
 	export let startDate;
 
 	export let endDate;
@@ -29,28 +27,6 @@ export let days = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","diman
 
 
 </script>
-
-<style>
-	/* main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	} */
-</style>
 
 
 <div>
