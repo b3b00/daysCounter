ba51f8e0d65b1dd871bc5474899883cf33729277
b3b00 <olivier.duhart@gmail.com>
2022-01-28 09:40:29 +0100

    .

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { writable } from 'svelte/store';
+
+export const netStatus = writable(true);
+
+
+
+const createWritableStore = (key, startValue) => {
+    const { subscribe, set, update } = writable(startValue);
+    
+      return {
+      subscribe,
+      update,
+      set,
+      useLocalStorage: () => {
+        const json = localStorage.getItem(key);
+        if (json) {
+          set(JSON.parse(json));
+        }
+        
+        subscribe(current => {
+          localStorage.setItem(key, JSON.stringify(current));
+        });
+      }
+    };
+  }
+  
+  export const leaves = createWritableStore('leaves',[]);
+
+  
